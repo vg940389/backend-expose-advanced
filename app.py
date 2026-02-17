ab9d7d907945dc3d89d6a9143b8a39cf3554e366
Venkat Golakoti <venkat.g93@gmail.com>
2026-02-17 16:48:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('whitelabel.html', error_code=404, message="Page Not Found"), 404
-
-@app.errorhandler(500)
-def internal_server_error(error):
-    return render_template('whitelabel.html', error_code=500, message="Internal Server Error"), 500
 
 @app.route('/api/hello')
 def hello_world():
